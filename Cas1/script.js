50f030070b42e3f19bc1a908de15fc6c2ca36bdb
Krajcik Marina <120725089+Kekso7@users.noreply.github.com>
2025-04-04 07:37:32 +0200

    Add files via upload

---
+++ Cas1/script.js
@@ -0,0 +1,38 @@
+
+// podatak koji se zove name, a njegova vrednost je Marina
+let name = "Marina"; // string
+let age = 33; // number - integer
+let height = 175.5; // float - decimal
+let isProgrammerc = true; // bool, boolean (true,false), true 1, false 0
+
+console.log(name, age, height, isProgrammerc);
+
+// napraviti 2 varijable, ime i prezime, napravite 3 varijablu ime plus prezime
+
+let firstName = "Marina";
+let lastName = "Krajcik";
+let firstNameAndLastName = firstName+ " " +lastName;
+console.log(firstNameAndLastName);
+
+
+// napraviti 2 varijable, prviBroj = 10 i drugi broj = 15, 3 varijablu zbir
+
+let firstNumber = 10;
+let secondNumber = 15;
+let sum = firstNumber+secondNumber;
+console.log(sum);
+
+let cena = 20;
+let kursEura = 117.1;
+
+let cenaJogurta = 50;
+
+let dinari = cena*kursEura;
+let jogurtDinari = cenaJogurta*kursEura;
+console.log(dinari,jogurtDinari);
+
+// Array
+// Napraviti array sa 3 stavke: Racunar, tastatura, mis
+
+let computerAccessories = ["Computer", "Keyboard", "Mouse"];
+console.log(computerAccessories);
