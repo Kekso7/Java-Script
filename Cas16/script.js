64662e911943a128a5a37da573d758d36349b2cc
Krajcik Marina <120725089+Kekso7@users.noreply.github.com>
2025-04-22 21:03:33 +0200

    Add files via upload

---
+++ Cas16/script.js
@@ -0,0 +1,90 @@
+
+let recipeHolder = document.getElementById('recipes');
+let tagsHolder = document.getElementById('tagsHolder');
+
+let params = new URLSearchParams(window.location.search);
+let category = params.get("category");
+
+if(category === null) {
+    fetch("https://dummyjson.com/recipes?limit=9&sortBy=name&order=asc")
+        .then(response => response.json())
+        .then(function(data) {
+
+            for(let recipe of data.recipes) {
+                appendCookingRecipe(recipe);
+            }
+        });
+} else {
+    let apiUrl = 'https://dummyjson.com/recipes/tag/'+category;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(function(data) {
+            recipeHolder.innerHTML = '';
+            for(let recipe of data.recipes) {
+                appendCookingRecipe(recipe);
+            }
+        })
+}
+
+fetch("https://dummyjson.com/recipes/tags")
+    .then(response => response.json())
+    .then(function(data) {
+        for (let tag of data) {
+            let tagElement = document.createElement('option');
+            tagElement.innerText = tag;
+            tagElement.value = tag;
+            tagsHolder.append(tagElement);
+        }
+    })
+
+    tagsHolder.addEventListener('change', function() {
+        let apiUrl = 'https://dummyjson.com/recipes/tag/'+this.value;
+
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(function(data) {
+                recipeHolder.innerHTML = '';
+                for(let recipe of data.recipes) {
+                    appendCookingRecipe(recipe);
+                }
+        })
+    });
+
+function appendCookingRecipe(recipe) {
+    let cookingInstructions = buildInstructionsElements(recipe.instructions)
+    let singleRecipe = document.createElement("div");
+    let recipeTitle = document.createElement("H1");
+    recipeTitle.innerText = recipe.name;
+    let recipeCuisine = document.createElement("p");
+    recipeCuisine.innerText = recipe.cuisine;
+
+    let permalinkElement = document.createElement("a");
+    permalinkElement.innerText = "Show recipe";
+    permalinkElement.href = "permalink.html?id="+recipe.id;
+
+    singleRecipe.append(recipeTitle, recipeCuisine, cookingInstructions, permalinkElement);
+    recipeHolder.append(singleRecipe);
+}
+
+function buildInstructionsElements(instructions) {
+    let recipeInstructions = document.createElement("ul");
+
+    for(let instruction of instructions) {
+        let instructionElement = document.createElement("li");
+        instructionElement.innerText = instruction;
+        recipeInstructions.append(instructionElement);
+    }
+    return recipeInstructions;
+}
+
+
+
+
+
+
+
+
+
+
+
